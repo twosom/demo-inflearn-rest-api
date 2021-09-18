2750c654b734bbcc9499adc5a1907f7463f1e19f
desirel <two_somang@icloud.com>
2021-09-18 22:55:28 +0900

    project creation

---
+++ src/test/java/com/icloud/demoinflearnrestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icloud.demoinflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
